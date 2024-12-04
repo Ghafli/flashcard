ec80d999795351d85e23e2fc624598faf2c1138f
ghalfi <w.alghafli@gmail.com>
2024-12-04 21:01:30 +0300

    feat: Add basic Next.js app with test

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Flashcard App</title>
+        <meta name="description" content="A simple flashcard application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Flashcard App</h1>
+        <p>Start learning with flashcards!</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
